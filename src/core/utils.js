a582729bdb54221d563d35cf1cd1fdbcbd37828d
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-05-28 13:59:55 +0300

    style parser added

--- src/core/utils.js
+++ src/core/utils.js
@@ -5,3 +5,27 @@ export function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
+export function createStyle(styleObj) {
+  let inlineStyle = ''
+  for (const style in styleObj) {
+    if (Object.prototype.hasOwnProperty.call(styleObj, style)) {
+      inlineStyle += ` ${getStyleName(style)}: ${styleObj[style]};`
+    }
+  }
+  return `style="${inlineStyle}"`
+}
+
+export function getStyleName(camelCaseName) {
+  const charCheck = (item) => {
+    const lowItem = item.toLowerCase()
+    return item === item.toUpperCase() ?
+      '-' + lowItem :
+      lowItem
+  }
+
+  return camelCaseName
+      .trim()
+      .split('')
+      .map(charCheck)
+      .join('')
+}
