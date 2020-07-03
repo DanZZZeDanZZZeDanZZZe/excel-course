b0b946a092eb0e46b90b1bf1c040b244e4689809
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-07-03 10:40:09 +0300

    debounce wrapper added

--- src/core/utils.js
+++ src/core/utils.js
@@ -55,3 +55,15 @@ export function isEqual(a, b) {
   }
   return a === b
 }
+
+export function debounce(fn, wait) {
+  let timeout
+  return function(...args) {
+    const later = () => {
+      clearTimeout(timeout)
+      fn(...args)
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+  }
+}
